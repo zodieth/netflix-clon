84ac55e2b06eb4f9943fe348f8ddd7280390226a
zodieth <contact.prodzodi@gmail.com>
2023-02-27 05:40:17 -0300

    firebase config

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -32,7 +32,9 @@ function Row({ title, movies }: Props) {
       </h2>
       <div className="group relative md:ml-2">
         <ChevronLeftIcon
-          className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0  transition hover:scale-125 group-hover:opacity-100"
+          className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0  transition hover:scale-125 group-hover:opacity-100 ${
+            !isMoved && "hidden"
+          }`}
           onClick={() => handleClick("left")}
         />
         <div
@@ -44,7 +46,7 @@ function Row({ title, movies }: Props) {
           ))}
         </div>
         <ChevronRightIcon
-          className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"
+          className={`absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0  transition hover:scale-125 group-hover:opacity-100 `}
           onClick={() => handleClick("right")}
         />
       </div>
